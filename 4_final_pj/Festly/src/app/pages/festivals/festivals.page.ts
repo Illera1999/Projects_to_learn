0feb024de25461a83e672e772ad546e25c8d9773
Illera1999 <illerarodriguez1999@gmail.com>
2025-11-13 21:41:07 +0000

    Ya se muestra el mapa

--- 4_final_pj/Festly/src/app/pages/festivals/festivals.page.ts
+++ 4_final_pj/Festly/src/app/pages/festivals/festivals.page.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { GoogleMap } from '@capacitor/google-maps';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ENV_KEYS } from 'src/app/config/keys';
 
 @Component({
   selector: 'app-festivals',
@@ -11,24 +12,25 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
   styleUrls: ['./festivals.page.scss'],
   standalone: true,
   imports: [
-    IonContent, 
-    IonHeader, 
-    IonTitle, 
-    IonToolbar, 
-    CommonModule, 
-    FormsModule, 
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class FestivalsPage implements OnInit, AfterViewInit {
   map: GoogleMap | undefined;
-@ViewChild('map', { static: false }) mapRef!: ElementRef<HTMLElement>;
+  private apiKey = ENV_KEYS.googleMapsApiKey;
+  @ViewChild('map', { static: false }) mapRef!: ElementRef<HTMLElement>;
 
   constructor() { }
 
 
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   async ngAfterViewInit() {
     await this.initMap();
