f5af8615cf1fdd2744e54afdbd0ff7183deef4cf
Illera1999 <illerarodriguez1999@gmail.com>
2025-11-16 22:07:58 +0000

    Firebase primeros pasos

--- 4_final_pj/Festly/src/main.ts
+++ 4_final_pj/Festly/src/main.ts
@@ -4,8 +4,12 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
 
 import { provideHttpClient } from '@angular/common/http';
+import { FirebaseApp, initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -13,5 +17,7 @@ bootstrapApplication(AppComponent, {
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideHttpClient(), 
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
   ],
 });
